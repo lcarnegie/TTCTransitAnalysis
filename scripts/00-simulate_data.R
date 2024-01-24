39fced6d0e3d6a218bfbfe2d9d9b817f24c28167
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2024-01-23 22:57:59 -0500

    made some initial code for simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,59 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates TTC bus, subway, and streetcar 
+# data to be gathered from OpenDataToronto 
+# Author: Luca Carnegie
+# Date: 23 January 2024
 
-
-#### Workspace setup ####
+#Import relevant libraries
+install.packages("tidyverse")
+install.packages('janitor')
+install.packages("lubridate")
+install.packages("wakefield")
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
+library(lubridate)
+library(wakefield)
 
-#### Simulate data ####
-# [...ADD CODE HERE...]
+# Simulate the dataset with 150 observations
 
+set.seed(27)
 
+simulated_data <- 
+  tibble(
+    "Transport Genre" = sample(
+      x = c("Bus", "Streetcar", "Subway"),
+      size = 150,
+      replace = TRUE
+    ), 
+    "Date" = date_stamp(
+      150,
+      random = FALSE,
+      x = NULL,
+      start = "2023-01-01",
+      k = 12,
+      by = "-1 months",
+      prob = NULL,
+      name = "Date"
+    ),
+    "Time" = time_stamp(
+      150,
+      x = seq(0, 23, by = 1),
+      prob = NULL,
+      random = FALSE,
+      name = "Time"
+    ),
+    "Location" = sample(
+      x = c("Bloor-Yonge Station", "Bay St. and King St.", "York Mills Station", "Queen and Church"),
+      size = 150,
+      replace = TRUE
+    ),
+    "Reason of Delay" = sample(
+      x = c("Mechanical", "Operations", "General Delay", "Emergency"),
+      size = 150,
+      replace = TRUE
+    ),
+    "Delay Time (mins)" = sample(1:47, 150, replace=TRUE)
+    
+  )
 
+# 
