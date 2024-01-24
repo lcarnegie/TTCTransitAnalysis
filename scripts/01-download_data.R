57b2682d01b55e93b272fab573a0e1dcd62aaa85
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2024-01-24 01:14:06 -0500

    Finally got my data downloaded! This is going to be a rough night.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,37 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from OpenDataToronto
+# Author: Luca Carnegie
+# Date: 23 Jan 2023
+# Contact: luca.carnegie@mail.utoronto.ca
 
 
 #### Workspace setup ####
 library(opendatatoronto)
+library(readxl)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# get all the bus data
+bus_data <- list_package_resources("e271cdae-8788-4980-96ce-6a5c95bc6618") |>
+  filter(name == "ttc-bus-delay-data-2023") |>
+  get_resource()
+
+#subway data 
+subway_data <- list_package_resources("996cfe8d-fb35-40ce-b569-698d51fc683b") |>
+  filter(name == "ttc-subway-delay-data-2023") |>
+  get_resource()
+  
+#get Streetcar Data
+streetcar_data <-list_package_resources("b68cb71b-44a7-4394-97e2-5d2f41462a5d") |>
+  filter(name == "ttc-streetcar-delay-data-2023") |>
+  get_resource()
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(bus_data, "inputs/data/bus_data.csv") 
+write_csv(subway_data, "inputs/data/subway_data.csv") 
+write_csv(streetcar_data, "inputs/data/streetcar_data.csv") 
 
          
