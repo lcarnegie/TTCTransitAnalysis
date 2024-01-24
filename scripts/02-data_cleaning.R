a0ee3ae4047266c06e25306f6376e00358690581
Luca Carnegie <41924665+lcarnegie@users.noreply.github.com>
2024-01-24 05:47:01 -0500

    somehow, still awake. I HAVE A CLEAN DATASET!!!

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,107 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Prerequisites: 01-download_data.R
+# Purpose: Cleans the raw TTC data downloaded from OpenDataToronto
+# Author: Luca Carnegie 
+# Date: January 24, 2024
+# Contact: luca.carnegie@mail.utoronto.ca
+
+
 
 #### Workspace setup ####
 library(tidyverse)
+library(janitor)
+library(lubridate)
+
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data_bus <- read_csv("inputs/data/bus_data.csv")
+raw_data_streetcar <- read_csv("inputs/data/streetcar_data.csv")
+raw_data_subway <- read_csv("inputs/data/subway_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+#Add vehicle column 
+raw_data_subway$vehicle <- "Subway"
+raw_data_streetcar$vehicle <- "Streetcar"
+raw_data_bus$vehicle <- "Bus"
+
+# from https://www.listendata.com/2015/06/r-keep-drop-columns-from-data-frame.html 
+
+## Subway 
+raw_data_subway <- raw_data_subway |> select(-Code, -Bound, -Vehicle, -`Min Gap`)
+raw_data_subway$vehicle <- "Subway"
+raw_data_subway$incident <- "NA"
+raw_data_subway <- rename(raw_data_subway, Location = Station)
+raw_data_subway <- raw_data_subway |> mutate(
+  Line =
+    case_match(
+      Line,
+      "YU" ~ "1",
+      "BD" ~ "2", 
+      "SRT" ~ "3", 
+      "SHP" ~ "4",
+      "YU / BD" ~ "1",
+      "BD/YU" ~ "2", 
+      "BLOOR DANFORTH & YONGE" ~ "2",
+      "YUS/BD" ~ "1",
+      "BD LINE 2" ~ "2", 
+      "999" ~ "NA",
+      "YUS" ~ "1",
+      "YU & BD" ~ "1",
+      "77 SWANSEA" ~ "NA"
+    )
+)
+
+raw_data_subway <- raw_data_subway |> select(Date, Time, Day, vehicle, Line, Location, incident, `Min Delay`)
+raw_data_subway <- rename(raw_data_subway, Incident = incident)
+raw_data_subway <- raw_data_subway |> mutate(Line = as.character(Line))
+
+
+## Streetcar
+raw_data_streetcar <- raw_data_streetcar |> select(-Bound, -Vehicle, -`Min Gap`)
+raw_data_streetcar <- raw_data_streetcar |> select(Date, Time, Day, vehicle, Line, Location, Incident, `Min Delay`)
+raw_data_streetcar <- raw_data_streetcar |> mutate(Line = as.character(Line))
+raw_data_streetcar
+
+## Bus
+raw_data_bus
+raw_data_bus <- raw_data_bus |> select(-Direction, -Vehicle, -`Min Gap`)
+raw_data_bus <- rename(raw_data_bus, Line = Route)
+raw_data_bus <- raw_data_bus |> select(Date, Time, Day, vehicle, Line, Location, Incident, `Min Delay`)
+raw_data_bus <- raw_data_bus |> mutate(Line = as.character(Line))
+raw_data_bus
+
+
+#Combine the three datasets 
+combined_data <- bind_rows(raw_data_bus, raw_data_streetcar, raw_data_subway)
+
+#Fix some categories
+combined_data <- combined_data |> mutate(
+  Incident =
+    case_match(
+      Incident,
+      "Diversion"  ~ "Diversion", 
+      "Security"  ~ "Security", 
+      "Cleaning - Unsanitary" ~ "Cleaning",
+      "Emergency Services"  ~ "Emergency Services", 
+      "Collision - TTC" ~ "Collision",
+      "Mechanical" ~ "Mechanical",
+      "Operations - Operator" ~ "Operations",
+      "Investigation" ~ "Investigation", 
+      "Utilized Off Route" ~ "Diversion",
+      "General Delay" ~ "General Delay", 
+      "Road Blocked - NON-TTC Collision" ~ "Collision",
+      "Held By" ~ "Held By" , 
+      "Vision" ~ "Vision", 
+      "Operations" ~ "Operations", 
+      "Collision - TTC Involved" ~ "Collision",
+      "Late Entering Service" ~ "Late Entering Service", 
+      "Overhead" ~ "Overhead", 
+      "Rail/Switches" ~ "Rail/Switches", 
+      "NA" ~ "NA"
+    )
+)
+
+combined_data
+  
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(combined_data, "outputs/data/cleaned_dataset.csv")
